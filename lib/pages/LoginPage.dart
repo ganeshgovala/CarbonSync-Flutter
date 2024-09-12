c265b7cf4c28ca5a58d40d16357c5704fc55243a
sandeep-gummala <23pa1a0580@vishnu.edu.in>
2024-09-12 12:09:09 +0530

    loginpage

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -21,20 +23,76 @@ class _LoginPageState extends State<LoginPage> {
               borderRadius: BorderRadius.circular(20),
               elevation: 20,
                child: Container(
-                height: 500,
-                width: 450,
+                height: 430,
+                width: 400,
                 decoration: BoxDecoration(
-                  color: Colors.red,
+                  color: Colors.white,
                   borderRadius: BorderRadius.circular(20)
                 ),
+                
                 child: Column(
-                  children: [
+                  children:[        
+                    SizedBox(height: 50,),                             
                     Row(
-                      children: [
-                        Image.asset(),
-                      ],
-                    
-                    )
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('lib/assets/emblem.png',height: 75,),
+                          SizedBox(width: 8,),
+                          Column(
+                            children: [
+                              Text("Ministry of",style: TextStyle(fontWeight: FontWeight.w600),),
+                              SizedBox(width: 0,),                  
+                              Text("COAL",style: TextStyle(fontSize: 28,color: const Color.fromARGB(255, 106, 31, 255),fontWeight: FontWeight.bold),)
+                            ],
+                          )
+                        ],
+                    ),
+                    SizedBox(height: 30,),
+                    SizedBox(
+                       height: 60,
+                       width: 300,
+                      child: TextField(                  
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "User ID",
+                          labelStyle: TextStyle(color: const Color.fromARGB(255, 184, 102, 228))
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25,),
+                    SizedBox(
+                       height: 60,
+                       width: 300,
+                      child: TextField(                  
+                        decoration: InputDecoration(
+                          
+                          border: OutlineInputBorder(borderSide: BorderSide(color: const Color.fromARGB(255, 206, 143, 251),width: 3)),
+                          labelText: "Password",
+                          labelStyle: TextStyle(color: const Color.fromARGB(255, 184, 102, 228))
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Center(
+                   child:
+                    MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Container(
+                        height: 52,
+                        width: 180,
+                         decoration: BoxDecoration(
+                           gradient: LinearGradient(
+                                  colors: [const Color.fromARGB(255, 184, 127, 255), const Color.fromARGB(255, 101, 5, 255)], // Gradient colors
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                              ),
+                               borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                           ),
+                        child: Center(child: Text("Login",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)),
+                       ),
+                    ),
+                   ),
                   ],
                 )
                ),
