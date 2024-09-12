00a2ddb36a0f853eb2816fb1d97a69045b326b34
ganeshgovala <sriramganesh28@gmail.com>
2024-09-12 10:02:01 +0530

    Pages Division

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors, sort_child_properties_last, prefer_const_constructors
-
+import 'package:carbonsync/pages/AnalyticsPage.dart';
+import 'package:carbonsync/pages/HomePage.dart';
+import 'package:carbonsync/pages/SettingsPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Dashboard',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -27,7 +29,6 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   int _currentIndex = 0;
 
-  // List of pages to show in the dashboard
   final List<Widget> _pages = [
     HomePage(),
     AnalyticsPage(),
@@ -84,36 +85,6 @@ class _DashboardState extends State<Dashboard> {
         ),
       ),
       body: _pages[_currentIndex], // Show the selected page
-    );
-  }
-}
-
-// Home Page
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page', style: TextStyle(fontSize: 24)),
-    );
-  }
-}
-
-// Analytics Page
-class AnalyticsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Analytics Page', style: TextStyle(fontSize: 24)),
-    );
-  }
-}
-
-// Settings Page
-class SettingsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Settings Page', style: TextStyle(fontSize: 24)),
     );
   }
 }
